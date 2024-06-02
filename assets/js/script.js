d0b3db82e522c1618d7fbab4bec3c9088f78cef5
Auschwalen <71735083+Auschwalen@users.noreply.github.com>
2024-06-02 16:27:27 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function clsbtn() {
+    document.getElementById("info").style.display = "none";
+    document.getElementById("info").style.animation = "fadeOut 1s";
+}
+
+function showinfo() {
+    document.getElementById("info").style.display = "flex";
+    document.getElementById("info").style.animation = "fadeIn 1s";
+}
